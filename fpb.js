4846d355567794df6dc00b5b6752dbaa38ae0d12
mfadhillah10 <fadhilakbarr1@gmail.com>
2018-03-13 18:10:13 +0700

    Week 4 Exercise 2

---
+++ fpb.js
@@ -0,0 +1,22 @@
+function fpb(angka1, angka2) {
+  
+    var hasil = 0;
+    // menampung hasil terbesar modulus i
+    for(var i = 1; i < angka1; i ++){
+      // // angka1 dan angka2 yang habis dibagi i itu hasilnya
+      if(angka1 % i === 0 && angka2 % i === 0) { 
+        hasil = i;
+      }
+    }
+    return hasil;
+    
+    // var angka = angka2%angka1;
+    // return angka;
+  }
+  
+  // TEST CASES
+  console.log(fpb(12, 16)); // 4
+  console.log(fpb(50, 40)); // 10
+  console.log(fpb(22, 99)); // 11
+  console.log(fpb(24, 36)); // 12
+  console.log(fpb(17, 23)); // 1
