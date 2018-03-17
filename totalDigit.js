05e9d84c010b08118a176fce875422e85fb8afe9
mfadhillah10 <fadhilakbarr1@gmail.com>
2018-03-17 22:03:39 +0700

    Week 4 Exercise 17

---
+++ totalDigit.js
@@ -0,0 +1,19 @@
+function totalDigitRekursif(angka) {
+    // you can only write your code here!
+    if (String(angka).length === 1){
+        return angka;
+    }
+    else {
+      var num = String(angka);
+      var tanpaBelakang = Number(num.slice(0,-1));
+      var numBelakang = Number(num[num.length-1]);
+      return numBelakang += totalDigitRekursif(tanpaBelakang);
+    }
+  }
+  
+  // TEST CASES
+  console.log(totalDigitRekursif(512)); // 8
+  console.log(totalDigitRekursif(1542)); // 12
+  console.log(totalDigitRekursif(5)); // 5
+  console.log(totalDigitRekursif(21)); // 3
+  console.log(totalDigitRekursif(11111)); // 5
